e527e913a123b04e658f2642ef3f36b207db941f
aasszzxx1 <112165862+aasszzxx1@users.noreply.github.com>
2022-08-26 04:17:56 -0700

    Delete jepthon/plugins directory

--- jepthon/plugins/التنصيب.py
+++
@@ -1,9 +0,0 @@
-from telethon import events
-import random, re
-from jepthon.utils import admin_cmd
-import asyncio 
-
-@borg.on(admin_cmd("تنصيب السورس"))
-async def _(event):
-     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("⌁︙**اهـلا بك فـي تنصيب السورس** \n⌁︙رابط التنصيب  -[اضغط هنا](⌁︙ يرجى متابعة قناة كريزون الرسمي لتنصيب السورس - @Krize_One1)\n⌁︙قناة السورس - @Krize_One1")
